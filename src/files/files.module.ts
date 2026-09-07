5879d7fd4c1ec33b4d591b18f17c27072f349e8b
CAS <alejo.hortua3@gmail.com>
2026-09-07 11:10:22 -0500

    Add module Auth

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [FilesController],
   providers: [FilesService],
   imports: [
-    ConfigModule
+    ConfigModule,
+    AuthModule,
   ]
 })
 export class FilesModule {}
