8d93ddef9e4144d09f5081ea8afc0757c9c90a38
CAS <alejo.hortua3@gmail.com>
2026-09-07 10:01:27 -0500

    Se quita endpoint seed

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -11,15 +11,15 @@ import { SeedService } from './seed.service';
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
-  @Get()
-  // @Auth( ValidRoles.admin )
-  @ApiOperation({
-    summary: 'Destruye y crea una nueva base de datos',
-    description:
-      'Destruye y crea la base de datos de productos y usuarios, esto invalida también tokens existentes y usuarios creados.',
-  })
-  @ApiResponse({ status: 200, description: 'Seed ejecutado con éxito' })
-  executeSeed() {
-    return this.seedService.runSeed();
-  }
+  // @Get()
+  // // @Auth( ValidRoles.admin )
+  // @ApiOperation({
+  //   summary: 'Destruye y crea una nueva base de datos',
+  //   description:
+  //     'Destruye y crea la base de datos de productos y usuarios, esto invalida también tokens existentes y usuarios creados.',
+  // })
+  // @ApiResponse({ status: 200, description: 'Seed ejecutado con éxito' })
+  // executeSeed() {
+  //   return this.seedService.runSeed();
+  // }
 }
